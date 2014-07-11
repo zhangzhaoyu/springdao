f5ce1a563db740e38dbadfc6cceabbfcf4fe4b1f
zhangzhaoyu <zhangzhaoyu0524@163.com>
2014-07-11 11:28:37 +0800

    update

--- src/main/java/cn/bjfu/springdao/jpa/dao/IGenericDao.java
+++ src/main/java/cn/bjfu/springdao/jpa/dao/IGenericDao.java
@@ -1,37 +1,98 @@
 /** 
- * Project Name:springdao 
+ * Project Name:fesdmp 
  * File Name:IGenericDao.java 
- * Package Name:cn.bjfu.springdao.hibernate.dao 
- * Date:2014年6月20日 下午4:28:36 
+ * Package Name:cn.bjfu.fesdmp.sys.dao 
+ * Date:2014年7月8日 下午10:04:42 
  * Copyright (c) 2014, zhangzhaoyu0524@163.com All Rights Reserved. 
  * 
 */  
   
 package cn.bjfu.springdao.jpa.dao;  
 
+import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
+
+import cn.bjfu.springdao.jpa.frame.IOrder;
+import cn.bjfu.springdao.jpa.frame.JoinMode;
 import cn.bjfu.springdao.utils.Pagination;
+
+
 /** 
  * ClassName:IGenericDao <br/> 
- * Function: IGenericDao. <br/> 
- * Reason:   IGenericDao. <br/> 
- * Date:     2014年6月20日 下午4:28:36 <br/> 
+ * Function: 通用DAO接口 <br/> 
+ * Reason:   通用DAO接口. <br/> 
+ * Date:     2014年7月8日 下午10:04:42 <br/> 
  * @author   zhangzhaoyu 
- * @version   
+ * @version   1.0
  * @since    JDK 1.7 
  * @see       
  */
 public interface IGenericDao<T> {
-	void insert(T t);
-	void delete(T t);
-	void update(T t);
-	T queryById(String id);
-	List<T> queryAll();
-	void executeSql(final String sql);
-	List<T> executeSql(final String sql, final Pagination<T> page);
-	List<T> executeHql(final String hql, final Pagination<T> page);
-	List<T> findByPage(final String hql, final Object[] values, final Pagination<T> page);
-	List<T> findByProperty(final String hql, final Object value);
-	List<T> findByCondition(final Object condition, final Object order, final Pagination<T> page);
+
+	public abstract void insert(T t);
+	public abstract void delete(T t);
+	public abstract void update(T t);
+	public abstract T findByKey(Serializable key);
+	/**
+	 * 
+	 * findAll:<br />
+	 * 查询所有
+	 *
+	 * @author zhangzhaoyu
+	 * @param order
+	 * 排序参数 可以为NULL
+	 * @return
+	 */
+	public abstract List<T> findAll(IOrder order);
+
+	/**
+	 * 
+	 * findByProperty:<br />
+	 * 按条件查询 map
+	 *
+	 * @author zhangzhaoyu
+	 * @param map
+	 * map 参数键值对的属性集合
+	 * @param order
+	 * 排序参数 可以为NULL
+	 * @return
+	 */
+	public abstract List<T> findByProperty(final Map<String, Object> map, IOrder order);
+	public abstract void findByPage(String jpql, Object[] values, Pagination<T> page, IOrder order);
+	/**
+	 * 
+	 * findByCondition:<br />
+	 * 所有参数均可为空，根据condition 的and 联结查询
+	 *
+	 * @author zhangzhaoyu
+	 * @param condition
+	 * 查询实体类
+	 * @param order
+	 * 排序
+	 * @param page
+	 * 分页
+	 * @return
+	 */
+	public abstract List<T> findByCondition(final Object condition, IOrder order, Pagination<T> page);
+	/**
+	 * 
+	 * findByCondition:<br />
+	 * 根据给定参数查询，所有参数均可为空，但是 condition 
+	 * 必须与 joinMode 同时出现 
+	 *
+	 * @author zhangzhaoyu
+	 * @param condition
+	 * 查询实体Bean
+	 * @param order
+	 * 排序字段
+	 * @param page
+	 * 分页字段
+	 * @param joinMode
+	 * 查询状态字段，取值为AND 或者 OR
+	 * @return
+	 */
+	public abstract List<T> findByCondition(final Object condition, IOrder order, Pagination<T> page, JoinMode joinMode);
+	
 }
  
--- src/main/java/cn/bjfu/springdao/jpa/dao/impl/AbstractGenericDao.java
+++ src/main/java/cn/bjfu/springdao/jpa/dao/impl/AbstractGenericDao.java
@@ -1,19 +1,19 @@
 /** 
- * Project Name:springdao 
+ * Project Name:fesdmp 
  * File Name:AbstractGenericDao.java 
- * Package Name:cn.bjfu.springdao.jpa.dao.impl 
- * Date:2014年6月29日 下午9:14:40 
+ * Package Name:cn.bjfu.fesdmp.sys.dao.impl 
+ * Date:2014年7月8日 下午10:17:35 
  * Copyright (c) 2014, zhangzhaoyu0524@163.com All Rights Reserved. 
  * 
 */  
   
 package cn.bjfu.springdao.jpa.dao.impl;  
 
-import java.beans.IntrospectionException;
-import java.beans.Introspector;
-import java.beans.PropertyDescriptor;
+import java.io.Serializable;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -22,21 +22,30 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.log4j.Logger;
+
 import cn.bjfu.springdao.jpa.dao.IGenericDao;
+import cn.bjfu.springdao.jpa.frame.IOrder;
+import cn.bjfu.springdao.jpa.frame.JoinMode;
 import cn.bjfu.springdao.utils.Pagination;
+
+
 
 /** 
  * ClassName:AbstractGenericDao <br/> 
- * Function: TODO ADD FUNCTION. <br/> 
- * Reason:   TODO ADD REASON. <br/> 
- * Date:     2014年6月29日 下午9:14:40 <br/> 
+ * Function: IGenericDao 的抽象实现类. <br/> 
+ * Reason:   IGenericDao 的抽象实现类. <br/> 
+ * Date:     2014年7月8日 下午10:17:35 <br/> 
  * @author   zhangzhaoyu 
- * @version   
+ * @version   1.0
  * @since    JDK 1.7 
  * @see       
  */
 public abstract class AbstractGenericDao<T> implements IGenericDao<T> {
 
+	private static final Logger logger = Logger.getLogger(AbstractGenericDao.class);
+	
 	private Class<T> entityClass;
 	
 	@PersistenceContext
@@ -46,13 +55,11 @@ public abstract class AbstractGenericDao<T> implements IGenericDao<T> {
 		this.entityClass = entityClass;
 	}
 	
-	public EntityManager getEntityManager() {
-		return this.em;
-	}
-	
 	@Override
 	public void insert(T t) {
-		this.em.persist(t);
+		if (t != null) {
+			this.em.persist(t);
+		}
 	}
 
 	@Override
@@ -66,111 +73,123 @@ public abstract class AbstractGenericDao<T> implements IGenericDao<T> {
 	}
 
 	@Override
-	public T queryById(String id) {
-		return this.em.find(entityClass, id);
+	public T findByKey(Serializable key) {
+		return this.em.find(entityClass, key);
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
-	public List<T> queryAll() {
-		String hql = "select d from " + entityClass.getSimpleName() +" d";
-		Query query = this.em.createQuery(hql);
+	public List<T> findAll(IOrder order) {
+		String jpql = " SELECT p FROM " + this.entityClass.getSimpleName() + " p ";
+		if (order != null) {
+			jpql += order.convertToSQL();
+		}
+		return this.em.createQuery(jpql).getResultList();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<T> findByProperty(final Map<String, Object> map, IOrder order) {
+		String jpal = "SELECT p FROM " + this.entityClass.getSimpleName() + " p ";
+		jpal += convertSqlFromMap(map);
+		if (order != null) {
+			jpal += convertToSQL(order);
+		}
+		Query query = this.em.createQuery(jpal);
 		return query.getResultList();
 	}
 
 	@SuppressWarnings("unchecked")
-	protected List<T> queryForObject(Map<String, Object> map) {
-		String sql = convertSqlFromMap(map);
-		Query query = this.em.createQuery(sql);
-		return query.getResultList();
-	}
-	
 	@Override
-	public void executeSql(String sql) {
-		// TODO Auto-generated method stub
+	public void findByPage(String jpql, Object[] values, Pagination<T> page, IOrder order) {
+		if (order != null) {
+			jpql += convertToSQL(order);
+		}
 		
-	}
-
-	@Override
-	public List<T> executeSql(String sql, Pagination<T> page) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<T> executeHql(String hql, Pagination<T> page) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<T> findByPage(String hql, Object[] values, Pagination<T> page) {
-		Query query = this.em.createQuery(hql);
+		Query query = this.em.createQuery(jpql);
 		setQueryParams(query, values);
 		page.setTotalRecord(query.getResultList().size());
 		List<T> result = query.setFirstResult(page.getOffset())
 				.setMaxResults(page.getPageSize()).getResultList();
 		
 		page.setDatas(result);
-		return result;
 	}
 
+	
 	@SuppressWarnings("unchecked")
 	@Override
-	public List<T> findByProperty(String hql, Object value) {
-		String sql = convertSqlFromMap(new HashMap());
-		Query query = this.em.createQuery(sql);
-		query.setParameter(0, value);
-		return query.getResultList();
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<T> findByCondition(Object condition, Object order, Pagination<T> page) {
-		
-		PropertyDescriptor props[] = null;
-		if (condition != null){
-			try {
-				props = Introspector.getBeanInfo(condition.getClass(),Object.class).getPropertyDescriptors();
-			} catch (IntrospectionException e) {
-				e.printStackTrace();
-			}
-			if(props != null){
-				String className = condition.getClass().getName();
-				String hql = "from "+ className +" where 1 = 1 ";
-				for (int i = 0; i < props.length; i++){
-					String name = props[i].getName();
-					String value;
-					try {
-						if(props[i].getReadMethod().invoke(condition) != null){
-							value =  (props[i].getReadMethod().invoke(condition)).toString();
-							if (value != null && !"".equals(value)){
-								hql +=  " and " + name + " = '" + value + "'";
-							}
-						}
-					} catch (IllegalArgumentException e) {
-						e.printStackTrace();
-					} catch (IllegalAccessException e){
-						e.printStackTrace();
-					} catch (InvocationTargetException e) {
-						e.printStackTrace();
-					}
-				}
-				Query query = this.em.createQuery(hql);
-				if (page != null){
-					page.setTotalRecord(query.getResultList().size());
-					List<T> result =  query.setFirstResult(page.getOffset()).setMaxResults(page.getPageSize()).getResultList();
-					return result;
-				}else{
-					return query.getResultList();
-				}
-			}
+	public List<T> findByCondition(Object condition, IOrder order, Pagination<T> page) {
+		String jpal = " SELECT p FROM "+ entityClass.getSimpleName() +" p WHERE 1 = 1 ";
+		if (condition != null) {
+			jpal += convertBeanToAndQL(condition);
 		}
-		return null;
+		if (order != null) {
+			jpal += convertToSQL(order);
+		}
+		Query query = this.em.createQuery(jpal);
+		if (page != null){
+			page.setTotalRecord(query.getResultList().size());
+			List<T> result =  query.setFirstResult(page.getOffset()).setMaxResults(page.getPageSize()).getResultList();
+			page.setDatas(result);
+			return result;
+		}else{
+			return query.getResultList();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<T> findByCondition(final Object condition, IOrder order, 
+			Pagination<T> page, JoinMode joinMode) {
+		String jpal = " SELECT p FROM "+ entityClass.getSimpleName() +" p ";
+		if (condition != null) {
+			jpal += convertBeanToJPAL(condition, joinMode);
+		} 
+		if (order != null) {
+			jpal += convertToSQL(order);
+		}
+		
+		logger.info(jpal);
+		Query query = this.em.createQuery(jpal);
+		if (page != null) {
+			page.setTotalRecord(query.getResultList().size());
+			List<T> result =  query.setFirstResult(page.getOffset()).setMaxResults(page.getPageSize()).getResultList();
+			page.setDatas(result);
+			return result;
+		}else{
+			return query.getResultList();
+		}
 	}
 	
-	private void setQueryParams(Query query, Object[] params) {  
+	/**
+	 * 
+	 * convertSqlFromMap:<br />
+	 * 将给定的Map 转换为 AND 连接的查询语句
+	 *
+	 * @author zhangzhaoyu
+	 * @param map
+	 * @return
+	 */
+	private String convertSqlFromMap(Map<String, Object> map) {
+		String sql = " WHERE 1 = 1 ";
+		StringBuilder builder = new StringBuilder(sql);
+		Set<String> set = map.keySet();
+		
+		for (String key : set) {
+			builder.append(" AND " + key +" = " + map.get(key));
+		}
+		return builder.toString();
+	}
+	
+	/**
+	 * 
+	 * setQueryParams:<br />
+	 * 设置查询的参数
+	 *
+	 * @author zhangzhaoyu
+	 * @param query
+	 * @param params
+	 */
+	private void setQueryParams(Query query, Object ... params) {  
         if (null == params) {  
             return;  
         }
@@ -179,16 +198,170 @@ public abstract class AbstractGenericDao<T> implements IGenericDao<T> {
         }
 	}
 	
-	private String convertSqlFromMap(Map<String, Object> map) {
-		String sql = "select p from " + this.entityClass.getSimpleName() + " p where 1 = 1 ";
-		StringBuilder builder = new StringBuilder(sql);
-		Set<String> set = map.keySet();
+	/**
+	 * 
+	 * getEntityManager:<br />
+	 * 获取实体管理器
+	 *
+	 * @author zhangzhaoyu
+	 * @return
+	 */
+	public EntityManager getEntityManager() {
+		return this.em;
+	}
+	
+	/**
+	 * 
+	 * convertBeanToJPAL:<br />
+	 * 将给定的Bean 按照JoinMode 拼接成JPQL
+	 * 注意 ： 本函数只能处理数值和字符串
+	 *
+	 * @author zhangzhaoyu
+	 * @param bean 
+	 * 不能为空
+	 * @param joinMode
+	 * 枚举类型取值为 AND 或者 OR
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	protected String convertBeanToJPAL(Object bean, JoinMode joinMode) {
+		Class clazz = bean.getClass();
+		Field[] fields = clazz.getDeclaredFields();
+		StringBuilder qlStirng = new StringBuilder();
 		
-		for (String key : set) {
-			builder.append("AND " + key +" = " + map.get(key));
+		if (joinMode == JoinMode.AND) {
+			qlStirng.append(" WHERE 1 = 1 ");
+			for (Field field : fields) {
+				String filedName = field.getName();
+				String firstLetter = filedName.substring(0, 1).toUpperCase();
+				String getMethodName = "get" + firstLetter + filedName.substring(1);
+				try {
+					Method getMethod = clazz.getMethod(getMethodName, new Class[]{});
+					Object value = getMethod.invoke(bean, new Object[]{});
+					
+					String type = field.getGenericType().toString();
+					int modifier = field.getModifiers();
+					// just private not final static 
+					if (Modifier.isPrivate(modifier) && !Modifier.isFinal(modifier) 
+							&& !Modifier.isStatic(modifier) && value != null) {
+						if (type.contains("String")) {
+							qlStirng.append(" AND p." + filedName + " like '%" + value + "%'");
+						} else if (type.contains("Integer") || type.contains("Long") 
+								|| type.contains("Short") || type.contains("Byte")) {
+							qlStirng.append(" AND p." + filedName + " = " + value);
+						}
+					} 
+				} catch (Exception e) {
+					logger.info("getMethod " + getMethodName +" not found!");
+				}
+			}
+			return qlStirng.toString();
 		}
-		
-		return builder.toString();
+		else if (joinMode == JoinMode.OR) {
+			qlStirng.append(" WHERE ");
+			int size = fields.length;
+			for (int i=0; i<size; i++) {
+				Field field = fields[i];
+				String filedName = field.getName();
+				String firstLetter = filedName.substring(0, 1).toUpperCase();
+				String getMethodName = "get" + firstLetter + filedName.substring(1);
+				try {
+					
+					Method getMethod = clazz.getMethod(getMethodName, new Class[]{});
+					Object value = getMethod.invoke(bean, new Object[]{});
+					
+					String type = field.getGenericType().toString();
+					int modifier = field.getModifiers();
+					// just private not final static 
+					 if (Modifier.isPrivate(modifier) && !Modifier.isFinal(modifier) 
+							&& !Modifier.isStatic(modifier) && value != null) {
+						if (type.contains("String")) {
+							qlStirng.append(" p." + filedName + " like '%" + value + "%' OR ");
+						} else if (type.contains("Integer") || type.contains("Long") 
+								|| type.contains("Short") || type.contains("Byte")
+								|| type.contains("Double") || type.contains("Float")
+								|| type.contains("Boolean")) {
+							qlStirng.append(" p." + filedName + " = " + value + " OR ");
+						}
+					}
+				} catch (Exception e) {
+					logger.info("getMethod " + getMethodName +" not found!");
+				}
+			}
+			String result = qlStirng.toString();
+			int index = result.lastIndexOf("OR");
+			return result.substring(0, index);
+		}
+		return null;
+	}
+	
+	/**
+	 * 
+	 * convertBeanToAndQL:<br />
+	 * 将一个实体Bean 转换成 AND 联结的JPAL
+	 * 暂时只处理数值类型、字符串和Bool
+	 *
+	 * @author zhangzhaoyu
+	 * @param bean
+	 * @return
+	 */
+	@SuppressWarnings("rawtypes")
+	protected String convertBeanToAndQL(Object bean) {
+		StringBuilder qlString = new StringBuilder();
+		try {
+			Map map = PropertyUtils.describe(bean);
+			Set keyset = map.keySet();
+			for (Object key : keyset) {
+				if (!key.toString().equals("class")) {
+					Object value = map.get(key);
+					Class typeClass = PropertyUtils.getPropertyType(bean, key.toString());
+					if (typeClass != null) {
+						String type = typeClass.toString();
+						if (value != null && type.contains("String")) {
+							qlString.append(" AND p." + key + " like '%" + value + "%'");
+						} else if (value != null && (type.contains("Integer") || type.contains("Long") 
+								|| type.contains("Short") || type.contains("Byte")
+								|| type.contains("Double") || type.contains("Float")
+								|| type.contains("Boolean"))) {
+							qlString.append(" AND p." + key + " = " + value);
+						}
+					}
+				}
+			}
+		} catch (IllegalAccessException | InvocationTargetException
+				| NoSuchMethodException e) {
+			logger.info("convertBeanToQL exception.");
+		}
+		return qlString.toString();
+	}
+	
+	/**
+	 * 
+	 * convertToSQL:<br />
+	 * 将排序转化为JPQL
+	 *
+	 * @author zhangzhaoyu
+	 * @param order
+	 * @return
+	 */
+	protected String convertToSQL(IOrder order) {
+		StringBuilder builder = new StringBuilder(" ORDER BY ");
+		Map<String, Object> map = order.getOrderByMap();
+		Set<String> set = map.keySet();
+		int size = set.size();
+		if (size == 1) {
+			for (String key : set) {
+				builder.append(" p." + key +" " + map.get(key));
+			}
+			return builder.toString();
+		} else if (size > 1) {
+			for (String key : set) {
+				builder.append(" p." + key  +" " + map.get(key) +" ,");
+			}
+			String result = builder.toString();
+			return result.substring(0, result.lastIndexOf(","));
+		}
+		return null;
 	}
 }
  
